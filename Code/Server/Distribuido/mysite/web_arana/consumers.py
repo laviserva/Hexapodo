b2ad163947a7209ad142161fa960d89e69a96f03
Emmanuel <emmanuelcv42@gmail.com>
2024-04-26 09:15:03 -0600

    se hizo asincrono el servidor

--- Code/Server/Distribuido/mysite/web_arana/consumers.py
+++ Code/Server/Distribuido/mysite/web_arana/consumers.py
@@ -1,39 +1,58 @@
-import json 
+import base64
+import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncWebsocketConsumer
+import asyncio
+
 class ConsumidorVotacion(WebsocketConsumer):
-    def connect(self):
+    async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
         print("conectasdo chat")
         self.accept()
-    def disconnect(self, code):
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
 
-        )
-    def receive(self, text_data):
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+        
+
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name, {"type": "chat.message", "message": message}
         )
-    def chat_message(self, event):
+
+    async def chat_message(self, event):
         message = event["message"]
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))    
-class ConsumidorIMG(WebsocketConsumer):
-    def connect(self):
+        await self.send(text_data=json.dumps({"message": message}))
+
+
+class ConsumidorIMG(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
+        
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         print("conectado IMG")
         self.accept()
+        self.running = True
+        while self.running:
+            image_path = r'C:\Users\Pc-01\Documents\Hexapodo\Code\Server\Distribuido\mysite\web_arana\static\imagen.jpg'
+            with open(image_path, 'rb') as image_file:
+                encoded_string = base64.b64encode(
+                    image_file.read()).decode('utf-8')
+            await self.send(text_data=encoded_string)
+
+            await asyncio.sleep(2)
+    async def disconnect(self, close_code):
+        self.running = False
+
+ 
